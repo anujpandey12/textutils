1c03f2009cfe8896a4fffcaec61ef00df9b3bf03
anujpandey12 <anujpan12@gmail.com>
2023-08-24 17:55:41 +0530

    added margin

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -40,10 +40,10 @@ export default function TextForm(props) {
                 <div className="mb-3">                
                     <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'black'}} value={text} onChange={handleOnChange} id="textBox" rows="8"></textarea>
                 </div>
-                <button disabled={text.length===0} className="btn btn-primary mx-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button disabled={text.length===0} className="btn btn-warning mx-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button disabled={text.length===0} className="btn btn-success mx-1" onClick={copyToClipBoard}>Copy Text</button>
-                <button disabled={text.length===0} className="btn btn-danger mx-1" onClick={handleClearClick}>Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-success mx-1 my-1" onClick={copyToClipBoard}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
             </div>
             <div className="my-3" style={{color: props.mode==='dark'?'white':'black'}}>
                 <h3>Your Text Summary</h3>
