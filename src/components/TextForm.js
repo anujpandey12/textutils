d2d6ead9ea95b5a61b0a2e62190ffac10bebda59
Anuj Pandey <anuj.pandey@prosares.com>
2025-07-15 15:35:36 +0530

    testing by changing the text and css

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -31,25 +31,72 @@ export default function TextForm(props) {
     }    
 
     return (
-        <>
-            <div style={{color: props.mode==='dark'?'white':'black'}}>
-                <h4>{props.heading}</h4>
-                <div className="mb-3">                
-                    <textarea className="form-control" style={{backgroundColor: props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'black'}} value={text} onChange={handleOnChange} id="textBox" rows="8"></textarea>
-                </div>
-                <button disabled={text.length===0} className="btn btn-primary mx-1 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button disabled={text.length===0} className="btn btn-warning mx-1 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button disabled={text.length===0} className="btn btn-success mx-1 my-1" onClick={copyToClipBoard}>Copy Text</button>
-                <button disabled={text.length===0} className="btn btn-danger mx-1 my-1" onClick={handleClearClick}>Clear Text</button>
-            </div>
-            <div className="my-3" style={{color: props.mode==='dark'?'white':'black'}}>
-                <h3>Your Text Summary</h3>
-                <p>{text.split(/\s+/).filter((element)=>{return element.length!==0}).length} words and {text.length} characters</p>
-            </div>
-            <div className="my-3" style={{color: props.mode==='dark'?'white':'black'}}>
-                <h3>Preview</h3>
-                <p>{text.length>0?text:'Nothing to preview!'}</p>
-            </div>
-        </>
-    )
+      <>
+        <div style={{ color: props.mode === "dark" ? "white" : "black" }}>
+          <h4>{props.heading}</h4>
+          <div className="mb-3">
+            <textarea
+              className="form-control"
+              style={{
+                backgroundColor: props.mode === "dark" ? "#042743" : "white",
+                color: props.mode === "dark" ? "white" : "black",
+              }}
+              value={text}
+              onChange={handleOnChange}
+              id="textBox"
+              rows="8"
+            ></textarea>
+          </div>
+          <button
+            disabled={text.length === 0}
+            className="btn btn-primary mx-1 my-1"
+            onClick={handleUpClick}
+          >
+            Convert to Uppercase
+          </button>
+          <button
+            disabled={text.length === 0}
+            className="btn btn-warning mx-1 my-1"
+            onClick={handleLoClick}
+          >
+            Convert to Lowercase
+          </button>
+          <button
+            disabled={text.length === 0}
+            className="btn btn-success mx-1 my-1"
+            onClick={copyToClipBoard}
+          >
+            Copy Text
+          </button>
+          <button
+            disabled={text.length === 0}
+            className="btn btn-danger mx-1 my-1"
+            onClick={handleClearClick}
+          >
+            Clear Text
+          </button>
+        </div>
+        <div
+          className="my-3"
+          style={{ color: props.mode === "dark" ? "white" : "black" }}
+        >
+          <h3 style={{ fontSize: "16px" }}>Your Text Summary</h3>
+          <p>
+            {
+              text.split(/\s+/).filter((element) => {
+                return element.length !== 0;
+              }).length
+            }{" "}
+            words and {text.length} characters
+          </p>
+        </div>
+        <div
+          className="my-3"
+          style={{ color: props.mode === "dark" ? "white" : "black" }}
+        >
+          <h3 style={{ fontSize: "16px" }}>Preview</h3>
+          <p>{text.length > 0 ? text : "Nothing to preview!"}</p>
+        </div>
+      </>
+    );
 }
